26f663dd44568e832077ed7ba825ab3a42c76d68
hockgod520 <liyp520@foxmail.com>
2025-12-19 14:42:11 +0800

    修改SecretManager中字段名，保持与Excel一致，

--- app/src/main/java/com/lefu/ppblutoothkit/SecretManager.kt
+++ app/src/main/java/com/lefu/ppblutoothkit/SecretManager.kt
@@ -8,14 +8,16 @@ import com.lefu.ppbase.PPScaleHelper
  */
 object SecretManager {
 
-    val SECRET_TYPE_8 = "qqlnPbHg95UEFQgmJs+bep+IQjdvY4kK9wbCGEVQr2Vv2+t9PbOP8l6NCcPSxqtk"
-    val SECRET_TYPE_4 = "Jyn7dEtLTGS9J3+jmtczIergA7Wiz9iqFVNVmcxmmkNtIWiFTIrf1WWpGmkDYuVB"
+    //TwoLegs140,TwoArms140,TwoLegs240
+    val BodyCompositionDetection1 = "Jyn7dEtLTGS9J3+jmtczIergA7Wiz9iqFVNVmcxmmkNtIWiFTIrf1WWpGmkDYuVB"
+    //Body270
+    val BodyCompositionDetection2 = "qqlnPbHg95UEFQgmJs+bep+IQjdvY4kK9wbCGEVQr2Vv2+t9PbOP8l6NCcPSxqtk"
 
     fun getSecret(calculateType: Int): String {
         if (PPScaleHelper.isCalcute8(calculateType)) {
-            return SECRET_TYPE_8
+            return BodyCompositionDetection2
         } else {
-            return SECRET_TYPE_4
+            return BodyCompositionDetection1
         }
     }
 
