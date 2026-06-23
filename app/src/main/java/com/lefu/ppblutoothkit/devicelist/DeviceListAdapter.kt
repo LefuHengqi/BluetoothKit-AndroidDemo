d2e866065e850ef57ae52033525aa6e214c3d656
hockgod520 <liyp520@foxmail.com>
2026-06-23 10:47:53 +0800

    增加Product和CalculateType显示

--- app/src/main/java/com/lefu/ppblutoothkit/devicelist/DeviceListAdapter.kt
+++ app/src/main/java/com/lefu/ppblutoothkit/devicelist/DeviceListAdapter.kt
@@ -6,6 +6,7 @@ import com.chad.library.adapter.base.BaseViewHolder
 import com.lefu.ppbase.PPDeviceModel
 import com.lefu.ppbase.PPScaleDefine
 import com.lefu.ppblutoothkit.R
+import com.lefu.ppcalculate.calcute.CalculateUtil
 import java.util.Locale
 
 class DeviceListAdapter : BaseQuickAdapter<DeviceVo?, BaseViewHolder?>(R.layout.activity_scan_list_item) {
@@ -20,10 +21,14 @@ class DeviceListAdapter : BaseQuickAdapter<DeviceVo?, BaseViewHolder?>(R.layout.
                     val device_type = holder.getView<TextView>(R.id.device_type)
                     val device_AdvLen = holder.getView<TextView>(R.id.device_AdvLen)
                     val device_sign = holder.getView<TextView>(R.id.device_sign)
-                    nameText.setText("devName:" + deviceModel.deviceName)
+                    val device_productType = holder.getView<TextView>(R.id.device_productType)
+                    val device_calculateType = holder.getView<TextView>(R.id.device_calculateType)
+                    nameText.setText("DeviceName:" + deviceModel.deviceName)
                     macText.setText("Mac:" + deviceModel.deviceMac)
-                    device_AdvLen.setText("advLength: " + deviceModel.advLength)
-                    device_sign.setText("sign: " + deviceModel.sign)
+                    device_AdvLen.setText("AdvLength: " + deviceModel.advLength)
+                    device_sign.setText("Sign: " + deviceModel.sign)
+                    device_productType.setText("Product: " + CalculateUtil.getProduct(deviceModel.deviceCalcuteType.getType()))
+                    device_calculateType.setText("CalculateType: " + deviceModel.deviceCalcuteType.name)
 
                     device_rssi.setText(String.format(Locale.getDefault(), "RSSI: %d dBm", deviceModel.rssi))
                     device_type.setText("PeripheralType:" + deviceModel.getDevicePeripheralType())
