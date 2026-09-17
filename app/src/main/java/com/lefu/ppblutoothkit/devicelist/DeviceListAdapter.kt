192efab5703b2b4b0dff25d0f6ca96742c8baf3c
hockgod520 <liyp520@foxmail.com>
2026-09-17 15:32:21 +0800

    修改Demo扫描数据展示项列表，根据功能模块区分，便于工程师能够快速根据需要进行阅读。

--- app/src/main/java/com/lefu/ppblutoothkit/devicelist/DeviceListAdapter.kt
+++ app/src/main/java/com/lefu/ppblutoothkit/devicelist/DeviceListAdapter.kt
@@ -1,6 +1,8 @@
 package com.lefu.ppblutoothkit.devicelist
 
+import android.annotation.SuppressLint
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
@@ -12,6 +14,7 @@ import com.lefu.ppcalculate.calcute.CalculateUtil
 import java.util.Locale
 
 class DeviceListAdapter : BaseQuickAdapter<DeviceVo?, BaseViewHolder?>(R.layout.activity_scan_list_item) {
+    @SuppressLint("SetTextI18n")
     override fun convert(holder: BaseViewHolder?, deviceVo: DeviceVo?) {
         deviceVo?.let {
             holder?.let {
@@ -29,6 +32,13 @@ class DeviceListAdapter : BaseQuickAdapter<DeviceVo?, BaseViewHolder?>(R.layout.
                     val device_httpType = holder.getView<TextView>(R.id.device_httpType)
                     val device_supportADN = holder.getView<TextView>(R.id.device_supportADN)
                     val device_wifiProtocolType = holder.getView<TextView>(R.id.device_wifiProtocolType)
+                    val device_calculateAPI = holder.getView<TextView>(R.id.device_calculateAPI)
+
+                    val mWifiModeLL = holder.getView<LinearLayout>(R.id.mWifiModeLL)
+                    val mWifiModeTitleTV = holder.getView<TextView>(R.id.mWifiModeTitleTV)
+
+                    val mComputingLL = holder.getView<LinearLayout>(R.id.mComputingLL)
+                    val mComputingTitleTV = holder.getView<TextView>(R.id.mComputingTitleTV)
 
                     nameText.setText("DeviceName:" + deviceModel.deviceName)
                     macText.setText("Mac:" + deviceModel.deviceMac)
@@ -38,66 +48,37 @@ class DeviceListAdapter : BaseQuickAdapter<DeviceVo?, BaseViewHolder?>(R.layout.
                     if (deviceModel.deviceType == PPScaleDefine.PPDeviceType.PPDeviceTypeCF) {
                         //体脂秤
                         device_productType.setText("Product: " + CalculateUtil.getProduct(deviceModel.deviceCalcuteType.getType()))
-                        device_productType?.visibility = View.VISIBLE
                         device_calculateType.setText("CalculateType: " + deviceModel.deviceCalcuteType.name)
-                        device_calculateType?.visibility = View.VISIBLE
+                        device_calculateAPI.text = "CalculateAPI: ${getCalculateAPI(deviceModel)}"
+                        mComputingTitleTV?.visibility = View.VISIBLE
+                        mComputingLL?.visibility = View.VISIBLE
                     } else {
                         //非体脂秤
-                        device_productType?.visibility = View.GONE
-                        device_calculateType?.visibility = View.GONE
+                        mComputingTitleTV?.visibility = View.GONE
+                        mComputingLL?.visibility = View.GONE
                     }
 
                     device_rssi.setText(String.format(Locale.getDefault(), "RSSI: %d dBm", deviceModel.rssi))
                     device_type.setText("PeripheralType:" + deviceModel.getDevicePeripheralType())
 
                     // 显示bit5: 鉴权
-                    device_needAuth.setText(
-                        "NeedAuth: " + if (deviceModel.needAuth)
-                            mContext.getString(R.string.support)
-                        else
-                            mContext.getString(R.string.no_support)
-                    )
+                    device_needAuth.text = "NeedAuth: " + if (deviceModel.needAuth) "YES" else "NO"
 
-                    if (com.peng.ppscale.business.ble.PPScaleHelper.isFuncTypeWifi(deviceModel.deviceFuncType)) {
+                    if (PPScaleHelper.isFuncTypeWifi(deviceModel.deviceFuncType)) {
                         // 显示bit6: 服务器类型
                         device_httpType.setText(
-                            "HttpScheme: " + if (deviceModel.httpType == 1)
-                                "HTTPS"
-                            else
-                                "HTTP"
+                            "HttpScheme: " + if (deviceModel.httpType == 1) "HTTPS" else "HTTP"
                         )
-                        device_httpType?.visibility = View.VISIBLE
-
                         // 显示bit7: 无WIFI列表请求配网
                         device_supportADN.setText(
-                            "SupportADN: " + if (deviceModel.isSupportADN)
-                                mContext.getString(R.string.support)
-                            else
-                                mContext.getString(R.string.no_support)
+                            "SupportADN: " + if (deviceModel.isSupportADN) "YES" else "NO"
                         )
-                        device_supportADN?.visibility = View.VISIBLE
-                        device_wifiProtocolType.setText(
-                            "WifiProtocolType: " +
-                                    if (deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeV2) {
-                                        "V2.0"
-                                    } else if (deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeV3) {
-                                        "V3.0"
-                                    } else if (deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeV4) {
-                                        "V4.0"
-                                    } else if (PPScaleHelper.isTorre(deviceModel.deviceProtocolType.getType())
-                                        || PPScaleHelper.isBorre(deviceModel.deviceProtocolType.getType())
-                                        || deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeDorre
-                                       ) {
-                                        "Torre"
-                                    } else {
-                                        "Unknown"
-                                    }
-                        )
-                        device_wifiProtocolType?.visibility = View.VISIBLE
+                        device_wifiProtocolType.text = "WifiProtocolType: ${getWifiProtocalType(deviceModel)}"
+                        mWifiModeLL?.visibility = View.VISIBLE
+                        mWifiModeTitleTV?.visibility = View.VISIBLE
                     } else {
-                        device_httpType.visibility = View.GONE
-                        device_supportADN.visibility = View.GONE
-                        device_wifiProtocolType.visibility = View.GONE
+                        mWifiModeLL?.visibility = View.GONE
+                        mWifiModeTitleTV?.visibility = View.GONE
                     }
 
                 }
@@ -115,4 +96,49 @@ class DeviceListAdapter : BaseQuickAdapter<DeviceVo?, BaseViewHolder?>(R.layout.
             return false
         }
     }
+
+    private fun getWifiProtocalType(deviceModel: PPDeviceModel): String = if (deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeV2) {
+        "V2.0/V3.0 Protocol"
+    } else if (deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeV3) {
+        "V2.0/V3.0 Protocol"
+    } else if (deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeV4) {
+        "Torre/V4.0 Protocol"
+    } else if (PPScaleHelper.isTorre(deviceModel.deviceProtocolType.getType())
+        || PPScaleHelper.isBorre(deviceModel.deviceProtocolType.getType())
+        || deviceModel.deviceProtocolType == PPScaleDefine.PPDeviceProtocolType.PPDeviceProtocolTypeDorre
+    ) {
+        "Torre/V4.0 Protocol"
+    } else {
+        "Unknown"
+    }
+
+    /**
+     * AC Four-Electrode Algorithm
+     * DC Four-Electrode Algorithm
+     * DC Four-Electrode Algorithm v2.0
+     * Dual-Frequency AC Four-Electrode Algorithm
+     * AC Eight-Electrode Algorithm
+     * AC Eight-Electrode Algorithm Smooth
+     */
+
+    private fun getCalculateAPI(deviceModel: PPDeviceModel): String {
+        val type = deviceModel.deviceCalcuteType
+        return when (type) {
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeDirect -> "AC Four-Electrode Algorithm"
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate,
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeNormal -> "DC Four-Electrode Algorithm v2.0"
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate4_0 -> "DC Four-Electrode Algorithm"
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate4_1 -> "Dual-Frequency AC Four-Electrode Algorithm"
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8_0,
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8_1,
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8_2,
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8_3,
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8_4,
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8 -> "AC Eight-Electrode Algorithm"
+            PPScaleDefine.PPDeviceCalcuteType.PPDeviceCalcuteTypeAlternate8_5 -> "AC Eight-Electrode Algorithm Smooth"
+            else -> "DC Four-Electrode Algorithm"
+        }
+    }
 }
+
+
